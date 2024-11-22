226cb939b8a7e9a7f93455888aef175909cde40d
longtsing <356084306@qq.com>
2024-11-22 23:59:41 +0800

    修改了python版本要求

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ setup(
         'License :: OSI Approved :: GPL License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
