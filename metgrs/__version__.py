ab5e092354d340065f0559302782de4ded51cd32
longtsing <356084306@qq.com>
2024-12-07 19:30:36 +0800

    修改了版本

--- metgrs/__version__.py
+++ metgrs/__version__.py
@@ -6,7 +6,7 @@
 # ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝   ╚═╝   ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝
 
 
-VERSION = (0, 2, 5)
+VERSION = (0, 3, 1)
 
 __version__ = '.'.join(map(str, VERSION))
 __author__='longtsing'
